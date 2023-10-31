78e4df0aab5d160410dd6063d460f2b413735d38
anatoliy2221 <anatoliy2221@ukr.net>
2023-10-31 23:22:01 +0200

    task end

--- task-1.js
+++ task-1.js
@@ -1,6 +1,8 @@
 function makeTransaction(quantity, pricePerDroi) {
-console.log(`You ordered ${quantity} droids worth ${quantity * pricePerDroi} credits!`);
+  return `"You ordered ${quantity} droids worth ${
+    quantity * pricePerDroi
+  } credits!"`;
 }
-console.log(makeTransaction(5, 3000)); 
-console.log(makeTransaction(3, 1000)); 
-console.log(makeTransaction(10, 500)); 
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
